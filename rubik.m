d824fa0efc0a4e04936c70c5d27377f651254332
Michal Hozza <mhozza@gmail.com>
2012-11-09 23:21:35 +0100

    Pridana nejaka jednoducha funkcionalita Pozor kodil som to v octave, takze v matlabe to nemusi celkom fungovat. V pripade potreby treba editnut

--- rubik.m
+++ rubik.m
@@ -1,5 +1,15 @@
 function rubik(img)
 	I = rgb2gray(img);
-	I = edge(I);	
+	h = fspecial('average',3);
+	I = imfilter(I, h);
+	h = fspecial('unsharp');
+	I = medfilt2(I,[10,10],'symmetric');
+	figure;
 	imshow(I);
+	I = edge(I,'canny',4);		
+	I = dilate(I,ones(5,5));	
+	figure;
+	
+	imshow(I);
+#	[H,T,R] = hough
 end
